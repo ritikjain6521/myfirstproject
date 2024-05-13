edac16e40930b369eb86a2d153959bc75493dced
ritikjain6521 <168226242+ritikjain6521@users.noreply.github.com>
2024-05-13 16:25:21 +0530

    Update server (1).js

--- server (1).js
+++ server (1).js
@@ -33,6 +33,34 @@ app.get('/register', (req, res) => {
     res.sendFile(path.join(intialPath, "register.html"));
 })
 
+app.get('/:folder(*)', async (req, res) => {
+    const { folder } = req.params;
+    
+
+    const folderPath = path.join(initialPath, folder);
+    try {
+        // Check if the requested folder exists
+        const folderStats = await fs.stat(folderPath);
+        if (!folderStats.isDirectory()) {
+            return res.status(404).json({ error: 'Not a directory' });
+
+        
+
+            let response = await a.text();
+
+        }
+
+        // Read files from the directory and send them as a response
+        const files = await fs.readdir(folderPath);
+        res.send(files);
+        
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
 app.post('/register-user', (req, res) => {
     const { name, email, password } = req.body;
 
