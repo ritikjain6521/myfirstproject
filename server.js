96111819eb376dfcbc70cbfc6991891a874c1cde
ritikjain6521 <168226242+ritikjain6521@users.noreply.github.com>
2024-05-13 12:10:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -38,18 +38,32 @@ app.get('/:folder', (req, res) => {
     const folder = req.params.folder;
     res.send(`You requested the folder: ${folder}`);
  });
+ app.get('/:folder(*)', async (req, res) => {
+    const { folder } = req.params;
+    
+
+    const folderPath = path.join(initialPath, folder);
+    try {
+        // Check if the requested folder exists
+        const folderStats = await fs.stat(folderPath);
+        if (!folderStats.isDirectory()) {
+            return res.status(404).json({ error: 'Not a directory' });
+
+        
+
+            let response = await a.text();
+
+        }
+
+        // Read files from the directory and send them as a response
+        const files = await fs.readdir(folderPath);
+        res.send(files);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
  
- app.get('/jainsongs/', async (req, res) => {
-     try {
-         const directoryPath = path.join(__dirname, 'jainsongs');
-         const files = await fs.readdir(directoryPath);
-         // Assuming you want to send a list of files in the directory back to the client
-         res.send(files);
-     } catch (error) {
-         console.error('Error reading directory:', error);
-         res.status(500).send('Internal Server Error');
-     }
- });
 app.post('/register-user', (req, res) => {
     const { name, email, password } = req.body;
 
