c812bd4dfc6feb676ed0077b595fac3278ce8aa2
ritikjain6521 <168226242+ritikjain6521@users.noreply.github.com>
2025-02-14 09:31:41 +0530

    Update form.js

--- public/js/form.js
+++ public/js/form.js
@@ -23,7 +23,7 @@ const submitBtn = document.querySelector('.submit-btn');
 
 if(name == null){ // means login page is open
     submitBtn.addEventListener('click', () => {
-        fetch('/login-user',{
+        fetch('/login-users',{
             method: 'post',
             headers: new Headers({'Content-Type': 'application/json'}),
             body: JSON.stringify({
@@ -39,7 +39,7 @@ if(name == null){ // means login page is open
 } else{ // means register page is open
 
     submitBtn.addEventListener('click', () => {
-        fetch('/register-user', {
+        fetch('/register-users', {
             method: 'post',
             headers: new Headers({'Content-Type': 'application/json'}),
             body: JSON.stringify({
